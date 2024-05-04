cb79dfc3589f6767fc2e48b32d7c88e53dd1b28f
HarulArora <81470702+HarulArora@users.noreply.github.com>
2024-05-04 17:35:57 +0000

    dataTypes

--- 01_Basics/dataTypes.js
+++ 01_Basics/dataTypes.js
@@ -1,5 +1,18 @@
 // alert("Hello") // we are using nodejs, not browser 
-let name="Harul Arora"
+let naam="Harul Arora"
 let age=20
 let isLoggedIn=true
+let state
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null
+// undefined
+// symbol => unique
+
+console.table([naam, age, isLoggedIn, state])
+
+console.log(typeof naam)
 
