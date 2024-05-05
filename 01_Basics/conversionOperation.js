d65b0a81fc23b29b3405f29f9fd1b69f60440b8b
HarulArora <81470702+HarulArora@users.noreply.github.com>
2024-05-05 09:33:05 +0000

    conversionOperation

--- 01_Basics/conversionOperation.js
+++ 01_Basics/conversionOperation.js
@@ -1,5 +1,36 @@
-let score="33"
-let num=Number(score)
+// let score="33"
+// let num=Number(score)
+// console.log(num)
+// console.log(typeof num)
 
-console.log(num)
-console.log(typeof num)
+
+// let bool=true
+// let b=Number(bool)
+// console.log(b)
+// console.log(typeof b)
+
+
+// let string="Harul Arora"
+// let s=Number(string)
+// console.log(s)
+// console.log(typeof s)
+
+
+// let number=10
+// let n=String(number)
+// console.log(n)
+// console.log(typeof n)
+
+
+
+// **********************Operations**************************
+
+// let val=3
+// let negValue=-val
+// console.log(negValue)
+
+
+let str1="hello"
+let str2="hello"
+let str3=str1+str2
+console.log(str3)
