609a73eb462426e32049311dcf7748d0eebf6b11
HarulArora <81470702+HarulArora@users.noreply.github.com>
2024-05-05 11:31:42 +0000

    Numbers and Math in javascript

---
+++ 01_Basics/number_and_math.js
@@ -0,0 +1,30 @@
+// ---------------Numbers---------------------
+// const score = 400
+// console.log(score)
+// console.log(typeof score)
+
+// const balance = new Number(100)
+// console.log(balance)
+// console.log(typeof balance)
+// console.log(balance.toString())
+// console.log(balance.toFixed(2))
+// console.log(balance.toString().length)
+
+
+
+// -----------------Math--------------------
+// console.log(Math)
+// console.log(Math.abs(-1))
+// console.log(Math.round(4.6))
+// console.log(Math.floor(4.5))
+// console.log(Math.ceil(4.5))
+// console.log(Math.max(4,3))
+// console.log(Math.min(2,1))
+
+
+console.log((Math.random()*10) + 1)
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max-min+1))+min)
