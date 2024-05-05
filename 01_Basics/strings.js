d69cbbc87a7484c9ec54a2a26d8c490de8535c65
HarulArora <81470702+HarulArora@users.noreply.github.com>
2024-05-05 11:04:45 +0000

    strings

---
+++ 01_Basics/strings.js
@@ -0,0 +1,33 @@
+// const name = "Harul"
+// const repoCount = 50
+
+// console.log(name + repoCount + "Value")
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+
+// const gameName = new String("Chess-abc")
+// console.log(gameName[0])
+// console.log(gameName[3])
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('s'))
+
+// const newString = gameName.substring(0,4)
+// console.log(newString)
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString)
+
+// const newStringone = "      harul      "
+// console.log(newStringone)
+// console.log(newStringone.trim())
+
+
+// const url = "https://harul.com/harul%20arora"
+// console.log(url.replace('%20', '-'))
+
+
+const naam = "Harul Arora is a good boy"
+console.log(naam.split(' '))
