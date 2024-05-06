c157446a634a5191754de55a8ac70ed6e5ff162e
HarulArora <81470702+HarulArora@users.noreply.github.com>
2024-05-06 12:59:38 +0000

    making basic functions in javascript

---
+++ 03_Basics/functions.js
@@ -0,0 +1,54 @@
+// // // Basic function
+// // function sayMyName(){
+// //     console.log("H")
+// //     console.log("A")
+// //     console.log("R")
+// //     console.log("U")
+// //     console.log("L")
+// // }
+
+// // sayMyName()
+
+
+// // ----------------------------Add two numbers---------------------------------
+
+// // function add(a, b){
+// //     let result=a+b
+// //     return result
+// // }
+
+// // const result = add(10,20)
+// // console.log(result)
+
+
+// // --------------------------------------------------------------------------
+
+// // function loginUser(username){
+// //     return `${username} just logged in`
+// // }
+// // console.log(loginUser("Harul Arora"))
+
+
+// // function calculateCartPrice(...num1){
+// //     return num1
+// // }
+// // console.log(calculateCartPrice(100,200,300,400,500,600))
+
+
+// // --------------Passing Object in a function------------------------------
+
+// const user = {
+//     username : "Harul Arora", 
+//     price : 99 
+// }
+
+// function abc(anyuser){
+//     console.log(`Name of user is ${anyuser.username} and price is ${anyuser.price}`)
+// }
+
+// // abc(user)
+// // ----OR-----
+// abc({
+//     username : "Kakarot",
+//     price : 69
+// })
