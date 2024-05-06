b3e8084dffe489f8946c91983d8df1be5689f9d5
HarulArora <81470702+HarulArora@users.noreply.github.com>
2024-05-06 18:44:25 +0000

    for, while and do while loops

---
+++ 05_Iterations/one.js
@@ -0,0 +1,21 @@
+// ---------------for loop-------------------
+// const arr=[1,2,3,4,5]
+// for(let i=0; i<arr.length; i++){
+//     console.log(arr[i])
+// }
+
+
+// --------------------while loop-----------------------------
+// let i=0
+// while(i<10){
+//     console.log(i)
+//     i++
+// }
+
+
+// ---------------------do while loop----------------------------
+// let i=0
+// do{
+//     console.log(i)
+//     i++
+// }while(i<10)
