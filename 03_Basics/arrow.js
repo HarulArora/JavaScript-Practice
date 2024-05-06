d62f94c17233af4afb72bbdbcf88701162ecec4f
HarulArora <81470702+HarulArora@users.noreply.github.com>
2024-05-06 13:58:39 +0000

    arrow functions in javascript

---
+++ 03_Basics/arrow.js
@@ -0,0 +1,34 @@
+// const user = {
+//     username : "Harul", 
+//     money : 1000,
+
+//     welcomeMessage : function(){
+//         console.log(`${this.username}, welcome to website`)
+//         console.log(this)
+//     }
+// }
+
+// user.welcomeMessage()
+// user.username="Kakarot"
+// user.welcomeMessage()
+// console.log(this)
+
+// -------------------Basic Arrow function--------------------------------
+
+// const add = (num1, num2) => {
+//     return num1+num2
+// }
+// console.log(add(2,3))
+
+
+// ----------------------Another way---------------------------------------
+
+// const add = (num1, num2) => (num1+num2)
+// console.log(add(3,4))
+
+
+// -------------------------return object-------------------------
+
+// const add = (num1, num2) => ({username: "Harul"})
+// console.log(add(10,20))
+
