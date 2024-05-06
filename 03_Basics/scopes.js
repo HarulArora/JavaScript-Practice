b9e4d6aa86d41457e148955ce0d53aeac94a8a8c
HarulArora <81470702+HarulArora@users.noreply.github.com>
2024-05-06 13:28:04 +0000

    scopes in javascript

---
+++ 03_Basics/scopes.js
@@ -0,0 +1,55 @@
+// var c=300
+// let a=300
+// if(true){
+//     let a=10
+//     const b=20
+// }
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+
+// function one(){
+//     const username = "harul"
+//     function two(){
+//         const website = "youtube"
+//         console. log (username) ;
+//     }
+//     // console. tog (website)
+//     two()
+// }
+// one()
+
+
+// ----------------------------------------------------------------
+
+// if(true){
+//     const username = "Harul"
+//     if(username === "Harul") {
+//         const website = "youtube"
+//         console.log(username+website)
+//     }
+//     // console.log(website)           // it will not execute
+// }
+
+
+
+// ---------------Different ways to declare function---------------------------------------------------
+
+// 1
+// function addone(num){
+//     return num+1
+// }
+
+// addone(5)
+
+
+
+
+// // 2
+// const addTwo = function(num){
+//     return num+2
+// }
+
+// addTwo(5)
