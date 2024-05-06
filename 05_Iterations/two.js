c33dcb7623d2b4848240ac7a644f77536887ec0c
HarulArora <81470702+HarulArora@users.noreply.github.com>
2024-05-06 18:59:19 +0000

    for of, for in, for each loops

---
+++ 05_Iterations/two.js
@@ -0,0 +1,41 @@
+// ---------------------for of loop------------------------------
+
+// const arr=[1,2,3,4,5,6]
+// for(const i of arr){
+//     console.log(i)
+// }
+
+
+// ------------------for in--------------------------------------
+
+// const myobject={
+//     js: "javascript",
+//     cpp: "c++",
+//     rb: "ruby",
+//     swift: "swift by apple"
+// }
+
+// for(let i in myobject){
+//     console.log(myobject[i])
+// }
+// for(let i in myobject){
+//     console.log(i)
+// }
+
+
+
+// const arr=[12,233,343,41,534234,62]
+// for(let i in arr){
+//     console.log(arr[i])
+// }
+
+
+
+
+
+// ----------------------for each loop--------------------------------
+
+// const arr=["a", "b", "c", "d"]
+// arr.forEach(function(val){
+//     console.log(val)
+// })
