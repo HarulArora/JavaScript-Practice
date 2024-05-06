283b0417358068fb34182367013ecbdb2e97b82b
HarulArora <81470702+HarulArora@users.noreply.github.com>
2024-05-06 14:04:36 +0000

    IIFE

---
+++ 03_Basics/IIFE.js
@@ -0,0 +1,9 @@
+// // Immediately Invoked Function Expression (IIFE)
+
+// (function chai(){
+//     console.group("DataBase is connected")
+// })();
+
+// ((name)=>{
+//     console.log(`DB2 is connected ${name}`)
+// })("harul")
