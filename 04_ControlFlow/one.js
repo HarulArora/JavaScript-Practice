61e48c0f0a7d51fd97915ff695e5309f6f7a4acc
HarulArora <81470702+HarulArora@users.noreply.github.com>
2024-05-06 15:23:36 +0000

    control flow in javascript

---
+++ 04_ControlFlow/one.js
@@ -0,0 +1,47 @@
+//  if
+
+// if(true){
+//     console.log("Hello")
+// }
+
+
+// -------------------------if-else if-------------------------------------------------
+
+// const a=10
+// if(a<5){
+//     console.log("Answer is less than 5")
+// }
+// else if(a>5){
+//     console.log("Answer is greater than 5")
+// }
+
+
+// ------------------------------switch case----------------------------------------
+
+// const month=10
+// switch(month){
+//     case 3:
+//         console.log("month is 3")
+//         break;
+//     case 4:
+//         console.log("month is 4")
+//         break;
+//     default:
+//         console.log("nothing")
+// }
+
+
+// -----------------------Nullish Coalescing Operator (??) : null undefined---------------------------
+
+// let val1
+// val1= 5??11
+// let val2
+// val2= null??10
+
+
+// console.log(val1)
+// console.log(val2)
+
+
+const price =100
+price <=80 ? console.log("less than 80") : console.log("greater than 80")
