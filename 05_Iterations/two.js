f90a8f4c8ecf5521398132715f93dd42b1c7e7a2
HarulArora <81470702+HarulArora@users.noreply.github.com>
2024-05-07 06:55:12 +0000

    filter

--- 05_Iterations/two.js
+++ 05_Iterations/two.js
@@ -39,3 +39,15 @@
 // arr.forEach(function(val){
 //     console.log(val)
 // })
+
+
+
+// ------------------------------filter-----------------------------------------
+
+
+// const nums=[1,2,3,4,5,6,7,8,9,10]
+// const newNums=nums.filter((num)=> {
+//     return num>4
+// })
+
+// console.log(newNums)
