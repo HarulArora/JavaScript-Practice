3627206b2bff22538952c9ef51614d7c5ab3407d
HarulArora <81470702+HarulArora@users.noreply.github.com>
2024-05-09 20:59:21 +0000

    class and inheritance

--- OOPS/inheritance.js
+++ OOPS/inheritance.js
@@ -1,22 +1,51 @@
-const User = {
-    name: "Harul",
-    email: "abcabc@gmail.com"
-}
-const Teacher = {
-    makeVideo: True
-}
+// // const User = {
+// //     name: "Harul",
+// //     email: "abcabc@gmail.com"
+// // }
+// // const Teacher = {
+// //     makeVideo: True
+// // }
 
-const TeachingSupport = {
-    isAvailable: false
-}
+// // const TeachingSupport = {
+// //     isAvailable: false
+// // }
 
-const TASupport = {
-    makeAssignment: "JS Assignment",
-    fullTime: true,
-    __proto__: TeachingSupport
-}
+// // const TASupport = {
+// //     makeAssignment: "JS Assignment",
+// //     fullTime: true,
+// //     __proto__: TeachingSupport
+// // }
 
 
-// Teacher.__proto__ = User      // Old Syntax
+// // // Teacher.__proto__ = User      // Old Syntax
 
-Object.setPrototypeOf(TeachingSupport, Teacher)  // Modern Syntax
+// // Object.setPrototypeOf(TeachingSupport, Teacher)  // Modern Syntax
+
+
+// // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// class User {
+//     constructor(username) {
+//         this.username = username
+//     }
+
+//     logMe() {
+//         console.log(`Username is ${this.username}`)
+//     }
+// }
+
+// class teacher extends User {
+//     constructor(username, email, password) {
+//         super(username)
+//         this.email = email
+//         this.password = password
+//     }
+
+//     addCourse() {
+//         console.log("New Course was added")
+//     }
+// }
+
+// const chai = new teacher("Harul", "abc@abc", "123")
+
+// chai.addCourse()
